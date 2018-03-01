5fcde641af369c0c1a27b054d3e4b79c7025d503
tjschleining <5403656+tschleining@users.noreply.github.com>
2018-02-28 22:51:18 -0500

    Works with new Submit-LMData pattern

--- Public/Add-LMDeviceOneTimeSDT.ps1
+++ Public/Add-LMDeviceOneTimeSDT.ps1
@@ -3,35 +3,45 @@
         [CmdletBinding()]
         Param (
             [Parameter(ParameterSetName="DeviceID", Mandatory=$true)]
-            $DeviceID,
+            [int]
+            $deviceId,
             [Parameter(ParameterSetName="DeviceDisplayName", Mandatory=$true)]
             $DeviceDisplayName,
             [parameter(Mandatory=$true, ParameterSetName="DeviceID")]
             [parameter(Mandatory=$true, ParameterSetName="DeviceDisplayName")]
             [DateTime]
-            $StartDate,
+            $startDateTime,
             [parameter(Mandatory=$true, ParameterSetName="DeviceID")]
             [parameter(Mandatory=$true, ParameterSetName="DeviceDisplayName")]
             [DateTime]
-            $EndDate
+            $endDateTime
         )
         Confirm-LMPortalConnection
-        $sdtType = '1'
-        $type = 'DeviceSDT'
         $resource = 'SDT'
+        [string]$startDateEpoch = [Math]::Round((New-TimeSpan -start (Get-Date -Date "1/1/1970") -end $startDateTime).TotalMilliseconds)
+        [string]$endDateEpoch = [Math]::Round((New-Timespan -start (get-date -date "1/1/1970") -end $endDateTime).TotalMilliseconds)
+        #[Int64]$startDateEpoch = (get-date $startDateTime -UFormat %s).Replace((Get-Culture).NumberFormat.NumberDecimalSeparator, '')
+        #[Int64]$endDateEpoch = (get-date $endDateTime -UFormat %s).Replace((Get-Culture).NumberFormat.NumberDecimalSeparator, '')
 
-        [string]$startDateEpoch = [Math]::Round((New-TimeSpan -start (Get-Date -Date "1/1/1970") -end $StartDate).TotalMilliseconds)
-        [string]$endDateEpoch = [Math]::Round((New-Timespan -start (get-date -date "1/1/1970") -end $EndDate).TotalMilliseconds)
-
-        if ($DeviceID)
-            {
-                $data = '{"sdtType":' + $sdtType + ',"type":"' + $type + '","deviceId":' + $DeviceID + ',"startDateTime":' + $startDateEpoch + ',"endDateTime":' + $endDateEpoch + '}'
-            }
+        #Add One time SDT options for endpoint
+        $staticOptions = @{
+            "sdtType" = 1;
+            "type" = 'DeviceSDT'
+        }
         
-        if ($DeviceDisplayName)
-            {
-                $data = '{"sdtType":' + $sdtType + ',"type":"' + $type + '","deviceDisplayName":"' + $DeviceDisplayName + '","startDateTime":' + $startDateEpoch + ',"endDateTime":' + $endDateEpoch + '}'
-            }
+        #copy local function parameters to new variable
+        $data = $PSBoundParameters
 
-        submit-LMdata -resource $resource -Data $data
+        #Add staticOptions to data object
+        foreach ($h in $staticOptions.GetEnumerator()) {
+            $data.Add($($h.Name), $($h.Value))
+        }
+        #Add Epoch time to data object
+        $data["startDateTime"] = $startDateEpoch
+        $data["endDateTime"] = $endDateEpoch
+        $dataJson = ConvertTo-Json $data
+
+        $ResourcePath = Convert-LMResourcePath -resource $resource
+        Submit-LMData -resourcePath $ResourcePath -Data $dataJson
+        #submit-LMdata -resource $resource -Data $data
     }
